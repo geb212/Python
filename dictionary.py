3116463154335a542b45e77a6f9c58f78977d1af
Gabriele <99821979+geb212@users.noreply.github.com>
2023-03-02 16:56:52 +0100

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
+playcounts = [78, 29, 44, 21, 89, 5]
+
+plays = {key:value for key, value in zip(songs,playcounts)}
+print(plays)
+print()
+
+plays["Purple Haze"] = 1
+print(plays)
+print()
+
+plays["Respect"] = 94
+print(plays)
+print()
+
+library = {"The Best Songs": plays, "Sunday Feelings":{} }
+
+print(library)
